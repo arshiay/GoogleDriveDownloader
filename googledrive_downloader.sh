e4ae71bc0c5b455fb6e5db9606cd8a8abb93f1a3
arshiay <arshia.y@gmail.com>
2018-07-21 16:38:27 +0430

    Update googledrive_downloader.sh

--- googledrive_downloader.sh
+++ googledrive_downloader.sh
@@ -16,5 +16,5 @@ echo "your download start in 5 seconds"
 
 TE=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$FILEID" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')
 
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$TE&id=$FILEID" -O FILENAME
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$TE&id=$FILEID" -O $FILENAME
 rm -rf /tmp/cookies.txt
